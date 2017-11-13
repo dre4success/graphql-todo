78276c6e7b8a1f3926ddfd8ea2989fd677c2c4fe
Damilare <dre4success@live.com>
2017-11-13 12:08:38 +0100

    creating graphql resolvers

--- model/index.js
+++ model/index.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 mongoose.Promise = global.Promise;
+const dotenv = require('dotenv');
 
-dotenv.config({path: '.env'})
+dotenv.config({ path: '.env' });
 mongoose.set('debug', true);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useMongoClient: true
-}).catch(err => {
-  console.error('Mongoose Error', err.stack)
-  process.exit(1)
-})
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useMongoClient: true
+  })
+  .catch(err => {
+    console.error('Mongoose Error', err.stack);
+    process.exit(1);
+  });
 
 module.exports.Todo = require('./todo');
