eda04e129ffb688d17a2ec6b00b8033231b1f1d6
Damilare <dre4success@live.com>
2017-11-13 22:23:11 +0100

    resolving static files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressGraphQL = require('express-graphql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const schema = require('./schemas');
 
 const app = express();
@@ -16,6 +17,14 @@ app.use(
     graphiql: true
   })
 );
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 2020;
 
